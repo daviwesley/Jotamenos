009029ec2b785443660ed378344918d3998a5cf8
daviwesley <davi.wesley@icloud.com>
2019-10-20 23:15:54 -0300

    add comment and new tokens

--- jotamenos.py
+++ jotamenos.py
@@ -2,7 +2,7 @@ from lark import Lark
 
 grammar = '''
 start : main_class class_declaration*
-main_class : "class" IDENTIFIER "{" reserved_string reserved_string reserved_string reserved_string "(" reserved_string "[" "]" IDENTIFIER ")" "{" statement "}" "}"
+!main_class : "class" IDENTIFIER "{" reserved_string reserved_string reserved_string reserved_string "(" reserved_string "[" "]" IDENTIFIER ")" "{" statement "}" "}"
 class_declaration : "class" IDENTIFIER ( "extends"i IDENTIFIER )? "{" ( var_declaration )* ( method_declaration )* "}"
 var_declaration : type IDENTIFIER ";"
 method_declaration : "public"i type IDENTIFIER "(" ( type IDENTIFIER ( "," type IDENTIFIER )* )? ")" "{" ( var_declaration )* ( statement )* "return" expression ";" "}"
@@ -10,13 +10,13 @@ type : "int [ ]"
        | "boolean"
        | "int"
        | IDENTIFIER
-statement : "{" ( statement )* "}"
+!statement : "{" ( statement )* "}"
             | "if" "("expression ")" statement "else" statement
             | "while" "(" expression ")" statement
             | "System.out.println" "(" expression ")" ";"
             | IDENTIFIER "=" expression ";"
             | IDENTIFIER "[" expression "]" "=" expression ";"
-expression : expression ( "&&" | "<" | "+" | "-" | "*" ) expression
+!expression : expression ( "&&" | "<" | "+" | "-" | "*" ) expression
             | expression "[" expression "]"
             | expression "." "length"
             | expression "." IDENTIFIER "(" ( expression ( "," expression )* )? ")"
@@ -29,8 +29,9 @@ expression : expression ( "&&" | "<" | "+" | "-" | "*" ) expression
             | "new" IDENTIFIER "(" ")"
             | "!" expression
             | "(" expression ")"
-IDENTIFIER: WORD
-reserved_string: "int" | "public" | "void" | "main" | "static"
+IDENTIFIER:  /[a-zA-Z_][0-9a-zA-Z_]*/
+!reserved_string: "int" | "public" | "void" | "main" | "static"
+COMMENT : "/*"  /.*?/ "*/"  
 %import common.WORD
 %import common.DIGIT
 %import common.ESCAPED_STRING
@@ -38,9 +39,11 @@ reserved_string: "int" | "public" | "void" | "main" | "static"
 
 %ignore WS
 %ignore ESCAPED_STRING
+%ignore COMMENT
 '''
 
 test_code = '''class Teste{
+    /** um comentario **/
     public static void main(int[] a){
         System.out.println(10);
     }
